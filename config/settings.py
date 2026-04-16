8590061564d1f91d6b56f296a72b78cdc08ce4af
Abhiram4068 <abhiram4068@gmail.com>
2026-04-16 13:12:57 +0530

    chore: switch database configuration from SQLite to MySQL

--- config/settings.py
+++ config/settings.py
@@ -94,23 +94,23 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'fileshare_db',
-#         'HOST':'localhost',
-#         'PASSWORD':'12345678',
-#         'USER':'root',
-#         'PORT':'3306'
-#     }
-# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-        
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'fileshare_db',
+        'HOST':'localhost',
+        'PASSWORD':'12345678',
+        'USER':'root',
+        'PORT':'3306'
     }
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+        
+#     }
+# }
 
 
 # Password validation
