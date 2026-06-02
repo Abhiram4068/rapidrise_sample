a53926a99d22158bba49eaf2a9d37dc3e26c6e88
Abhiram S <abhiram4068@gmail.com>
2026-06-02 11:40:44 +0530

    Enable DEBUG mode based on environment variable

--- config/settings.py
+++ config/settings.py
@@ -31,7 +31,7 @@ load_dotenv(BASE_DIR / ".env")
 # SECURITY WARNING: keep the secret key used in production secret!
 
 SECRET_KEY = os.getenv("SECRET_KEY")
-
+DEBUG = os.getenv("DEBUG", "False") == "True"
 ALLOWED_HOSTS = [
     ".onrender.com"
 ]
